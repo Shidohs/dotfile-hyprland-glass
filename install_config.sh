3e259368cf27fa31d8838e7237692a1b9df414f4
Shidox <99781106+Shidohs@users.noreply.github.com>
2024-08-13 01:12:23 +0000

    Ajustando los requerimientos y mejorando el instalador

--- install_config.sh
+++ install_config.sh
@@ -15,7 +15,10 @@ case $change_dm in
     y|Y)
         # Instala SDDM y establece como DM
         sudo pacman -S sddm
-        sudo systemctl enable sddm.service --force
+       echo "Desactivando su DM predeterminado"
+sudo systemctl disable $(systemctl list-unit-files --type=service --state=enabled | grep 'dm\.service' | awk '{print $1}')
+echo "Activar SDDM como display manager (DM) predeterminado"
+sudo systemctl enable sddm.service
         chmod +x sddm-theme/sddm_install.sh
         ./sddm-theme/sddm_install.sh
         ;;
@@ -59,11 +62,137 @@ if [[ ! -f $requirements_file ]]; then
 fi
 
 # Instalar dependencias de cada sección
-for section in "Audio" "Desktop Environment" "Power Management" "Bluetooth" "Security" "Terminal" "Utils"; do
+for section in "Audio" "Desktop Environment" "Power Management" "Bluetooth" "Security" "Terminal" "Utils" "Fonts" "Sistema" "Otros" "Multimedia" "Web" "File" "Code"; do
   echo "Instalando paquetes de la sección $section..."
   packages=$(jq -r ".System[\"$section\"][]" $requirements_file)
   install_packages pacman $packages
 done
 
+echo "----- CAMBIANDO LA SHELL -----"
+
+chsh -s /bin/zsh
+
+echo "----- INSTALAND OHMYZSH -----"
+#instalar y configurar ohmyzsh
+if [ ! -f ~/.oh-my-zsh/oh-my-zsh.sh ]; then
+	echo "No está instalado Oh My Zsh, comenzará la instalación"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+	echo "Oh My Zsh ya está instalado"
+fi
+
+echo "creando enlace para root de'Oh My Zsh'"
+# Instalar oh-my-zsh como root
+sudo ln -s "$HOME"/.oh-my-zsh /root/
+echo "Enlazado"
+
+# tema pówerlevel10k
+echo "INSTALANDO TEMA POWERLEVEL10K"
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
+	echo "powerlevel10k no está instalado, comenzará a instalarse"
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+	echo "Modificando .p10k.zsh"
+	
+else
+	echo "powerlevel10k está instalado"
+	echo "Modificando .p10k.zsh"
+	
+fi
+
+echo "---------------------"
+echo "----- HACER ESTE PROCESO EN ROOT -----"
+
+echo "creando enlace para root de'aliases y .zshrc'"
+# ROOT
+sudo ln -s ~/.zshrc /root/
+sudo ln -s ~/.aliases /root/
+
+#Plugins
+echo "----- INSTALANDO PLUGINS EN USER -----"
+echo "Instalando plugins de ohmyzsh"
+# Verificar si los repositorios fzf-tab, zsh-syntax-highlighting y zsh-autosuggestions están clonados
+if [ -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab" ] &&
+	[ -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ] &&
+	[ -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+	echo "Los repositorios ya están clonados."
+else
+	# Clonar los repositorios fzf-tab, zsh-syntax-highlighting y zsh-autosuggestions
+	git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+
+echo "dependencias para el tema"
+if pacman -Q "qt5-graphicaleffects" &>/dev/null &&
+	pacman -Q "qt5-svg" &>/dev/null &&
+	pacman -Q "qt5-quickcontrols2" &>/dev/null; then
+	echo "Dependencias para sddm-theme: Instalados"
+else
+	sudo pacman -S --noconfirm qt5-graphicaleffects qt5-svg qt5-quickcontrols2
+	echo "Dependencias para sddm-theme: Instalados"
+fi
+
+if ! pacman -Qq "sddm" >/dev/null; then
+	echo "sddm: no está instalado"
+	echo "Instalando SDDM"
+	sudo pacman -S --noconfirm sddm
+fi
+
+# instalando el repo chaotic
+echo "----- INSTALANDO REPO CHAOTIC_EUR -----"
+
+# Instalar clave primaria
+sudo pacman -Sc --noconfirm
+sudo pacman-key --init
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+
+# Instalar keyring y mirrorlist
+if ! pacman -Qq chaotic-keyring &>/dev/null; then
+	sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+	sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+	sudo pacman -Sc --noconfirm
+fi
+
+# Configurar pacman.conf
+
+if [ -f "/etc/pacman.d/chaotic-mirrorlist" ]; then
+	if ! grep -q "\[chaotic-aur\]" /etc/pacman.conf; then
+
+		# Agregar sección [chaotic-aur]
+		echo '[chaotic-aur]' | sudo tee -a /etc/pacman.conf
+
+		# Agregar directiva Include
+		echo 'Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
+
+	fi
+else
+	echo "El archivo /etc/pacman.d/chaotic-mirrorlist no existe."
+fi
+
+echo "----- INSTALANDO TIENDA DE ARCH -----"
+#isntalando la tienda de arch
+
+if pacman -Qq "pamac-all"; then
+	echo "Tienda Aur: Instalado"
+	if pacman -Qq "powerpill"; then
+		sudo pacman -Sy --noconfirm && sudo powerpill -Su && yay -Su
+	else
+		yay -S --noconfirm powerpill
+		sudo pacman -Sy --noconfirm && sudo powerpill -Su && yay -Su
+	fi
+
+else
+	echo "Tienda Aur: No Instalado"
+	yay -S --noconfirm pamac-all
+	yay -S --noconfirm powerpill
+	sudo pacman -Sy --noconfirm && sudo powerpill -Su && yay -Su
+fi
+#Iniciando el servicio de bluetooth
+sudo systemctl start bluetooth
+sudo systemctl enable bluetooth
+
+echo "INSTALACION COMPLETADA"
 
 
